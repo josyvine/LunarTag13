ff5a7b1124ee3aa528257b12bd6e35a5b6733aa9
josyvine <64621103+josyvine@users.noreply.github.com>
2025-11-18 13:30:47 +0530

    Update SettingsFragment.java

--- app/src/main/java/com/lunartag/app/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/lunartag/app/ui/settings/SettingsFragment.java
@@ -13,7 +13,7 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.fragment.NavHostFragment; 
 
 import com.lunartag.app.R;
 import com.lunartag.app.databinding.FragmentSettingsBinding;
