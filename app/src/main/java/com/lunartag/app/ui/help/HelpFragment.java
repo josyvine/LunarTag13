51dfe03a0050279b20c8ac7197d7e38e743d0c9a
josyvine <64621103+josyvine@users.noreply.github.com>
2025-11-26 23:25:55 +0530

    Create HelpFragment.java

---
+++ app/src/main/java/com/lunartag/app/ui/help/HelpFragment.java
@@ -0,0 +1,20 @@
+package com.lunartag.app.ui.help;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.lunartag.app.R;
+
+public class HelpFragment extends Fragment {
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_help, container, false);
+    }
+}
